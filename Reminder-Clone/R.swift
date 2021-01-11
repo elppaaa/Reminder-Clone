9af132020254a8decf468cc105341f7332f1feb6
elppaaa <withbcd@gmail.com>
2021-01-11 14:02:24 +0900

    [feat] Add Color, Resources file (R.swift)

---
+++ Reminder-Clone/R.swift
@@ -0,0 +1,13 @@
+//
+// Created by JK on 2021/01/09.
+//
+
+import UIKit
+//swiftlint:disable type_name
+struct R {
+	struct color {
+		static let systemBackground = UIColor(named: "systemBackground")
+		static let applicationBackground = UIColor(named: "applicationBackground")
+		static let systemGray6 = UIColor(named: "systemGray6")
+	}
+}
---
+++ Reminder-Clone/UIColor+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+Extension.swift
+//  Reminder-Clone
+//
+//  Created by JK on 2021/01/06.
+//
+
+import UIKit
+
+public extension UIColor {
+  convenience init(r: Int, g: Int, b: Int, a: CGFloat) {
+    self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: a)
+  }
+  
+  convenience init(hex: Int) {
+    self.init(r: (hex & 0xff0000) >> 16, g: (hex & 0xff00) >> 8, b: (hex & 0xff), a: 1)
+  }
+}
+
+extension NSObject {
+	static var describe: String {
+    String(describing: self)
+  }
+}
