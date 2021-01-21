fcea667f058caebd29273f4e1608bb55d2e8b439
elppaaa <withbcd@gmail.com>
2021-01-21 12:17:27 +0900

    [feat] Add TableView on ViewController

--- Reminder-Clone/HomeTableList.swift
+++ Reminder-Clone/HomeTableList.swift
@@ -4,26 +4,155 @@
 
 import UIKit
 
+// TODO: - UITableView 완성할 것.
+/*
+ UITableView 에서, collapse 고려하여
+ stackview, collectionview, tableview 중에서
+ 어떤 뷰로 표시할 것인지를 고민 후 재 작성 예정.
+ */
+
+// MARK: -
 class HomeListTableView: UITableView {
+	let bindDataSource = HomeListTableDataSource()
+
+	required init?(coder: NSCoder) {
+		fatalError("ERROR WHEN CREATE TABLEVIEW")
+	}
+
+	override init(frame: CGRect, style: Style = .plain) {
+		super.init(frame: frame, style: style)
+		dataSource = bindDataSource
+		register(HomeListTableCell.self, forCellReuseIdentifier: HomeListTableCell.describe)
+		configLayout()
+		reloadData()
+	}
+
+	convenience init() {
+		self.init(frame: .zero)
+		dataSource = bindDataSource
+		register(HomeListTableCell.self, forCellReuseIdentifier: HomeListTableCell.describe)
+    configLayout()
+    reloadData()
+	}
+  
+  func configLayout() {
+    translatesAutoresizingMaskIntoConstraints = false
+    estimatedRowHeight = 80
+    rowHeight = 50
+    let tableViewHeight = CGFloat(50 * bindDataSource.data.count)
+    heightAnchor.constraint(equalToConstant: tableViewHeight).isActive = true
+    
+    backgroundColor = .none
+    showsVerticalScrollIndicator = false
+    showsHorizontalScrollIndicator = false
+    isScrollEnabled = false
+    layer.cornerRadius = 20
+  }
+  
+  #if DEBUG
+  @objc func injected() {
+    inject()
+  }
+  #endif
+  
+  func inject() {
+    //change vc
+    let vc = ViewController()
+    let navigation = UINavigationController(rootViewController: vc)
+    //swiftlint:disable force_unwrapping
+    UIApplication.shared.windows.first!.rootViewController = nil
+    //swiftlint:disable force_unwrapping
+    UIApplication.shared.windows.first!.rootViewController = navigation
+  }
 
 }
 
-class HomeListTableCell: UITableViewCell, HomeListCellViewType {
+//extension HomeListTableView: UITableViewDelegate {
+//
+//}
 
-	func configLayout() {
-
+// MARK: -
+class HomeListTableDataSource: NSObject, HomeListDataSource {
+	var data: [HomeRadiusList] {
+		_data
 	}
 
+	private var _data: [HomeRadiusList] = [
+		HomeRadiusList(title: "Today", icon: "folder.circle.fill", color: .systemBlue, count: 5),
+		HomeRadiusList(title: "Scheduled", icon: "calendar.circle.fill", color: .red, count: 9),
+		HomeRadiusList(title: "All", icon: "tray.circle.fill", color: .gray, count: 8),
+		HomeRadiusList(title: "Flagged", icon: "flag.circle.fill", color: .systemOrange, count: 7),
+		HomeRadiusList(title: "Flagged", icon: "flag.circle.fill", color: .systemOrange, count: 7),
+	]
+}
+
+extension HomeListTableDataSource: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    1
+  }
+//  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//    "My Lists"
+//  }
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		data.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeListTableCell.describe, for: indexPath)
+			as? HomeListTableCell else {
+			fatalError("ERROR WHEN CREATE CELL")
+		}
+		let data = _data[indexPath.row]
+		cell.configCell(with: data)
+		return cell
+	}
+}
+
+// MARK: -
+class HomeListTableCell: UITableViewCell, HomeListCellViewType {
+  private static let size: CGFloat = 40
 	required init?(coder: NSCoder) {
 		fatalError("ERROR")
 	}
 
+	override init(style: CellStyle, reuseIdentifier _: String? = nil) {
+		super.init(style: style, reuseIdentifier: Self.describe)
+		configLayout()
+	}
+
 	var titleLabel = UILabel.makeView(
-			font: .systemFont(ofSize: 15, weight: .bold))
+		font: .systemFont(ofSize: size / 2))
 
 	var countLabel = UILabel.makeView(
-		color: .gray,	font: .systemFont(ofSize: 10))
+    color: .gray,
+    font: .systemFont(ofSize: size / 2))
 
-	var iconView: UIImageView
+	var iconView: UIImageView = {
+    let imageView = UIImageView()
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleToFill
+    imageView.widthAnchor.constraint(equalToConstant: size).isActive = true
+    imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+		return imageView
+	}()
 
+	fileprivate func configLayout() {
+		translatesAutoresizingMaskIntoConstraints = false
+
+		accessoryType = .disclosureIndicator
+    
+    contentView.addSubview(iconView)
+    contentView.addSubview(titleLabel)
+    contentView.addSubview(countLabel)
+    _ = contentView.subviews.map { v in
+      v.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+    
+    NSLayoutConstraint.activate([
+      iconView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+      titleLabel.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 7),
+      countLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -3)
+    ])
+
+	}
 }
--- Reminder-Clone/ViewController.swift
+++ Reminder-Clone/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
   
   let controller = UISearchController(searchResultsController: nil)
   let collection = HomeListCollectionView()
+	let table = HomeListTableView()
   var searchBar = UISearchBar()
+
+  let mainStack: UIStackView = {
+    let stack = UIStackView()
+    stack.translatesAutoresizingMaskIntoConstraints = false
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    return stack
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,7 +33,7 @@ class ViewController: UIViewController {
   
   func searchBarSetting() {
     searchBar = controller.searchBar
-    searchBar.translatesAutoresizingMaskIntoConstraints = false
+//    searchBar.translatesAutoresizingMaskIntoConstraints = false
     searchBar.backgroundColor = .clear
     
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: nil)
@@ -33,19 +42,24 @@ class ViewController: UIViewController {
     navigationItem.searchController?.isActive = true
     navigationItem.hidesSearchBarWhenScrolling = true
   }
-  
+
   // MARK: - config Layout
   func configLayout() {
-    
     // collectionView
-    view.addSubview(collection)
+		mainStack.addArrangedSubview(collection)
+    mainStack.addArrangedSubview(table)
+
+    view.addSubview(mainStack)
     NSLayoutConstraint.activate([
-      collection.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
-      collection.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-      collection.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-      collection.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+      mainStack.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
+      mainStack.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: -10),
+      mainStack.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+      mainStack.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
     ])
     
+//    searchBar.leadingAnchor.constraint(equalTo: UINavigationBar.appearance().leadingAnchor).isActive = true
+//    searchBar.trailingAnchor.constraint(equalTo: UINavigationBar.appearance().trailingAnchor).isActive = true
+
     // ListView
     
   }
