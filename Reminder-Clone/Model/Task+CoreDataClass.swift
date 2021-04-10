e58e72b551fb5e98625e01c3d46632df56e1e408
elppaaa <withbcd@gmail.com>
2021-04-10 18:42:31 +0900

    [feat] Add toDictionary func
    - Task NSManagedObject 를 Dictionary 로 변환해주는 computed property 생성

--- Reminder-Clone/Model/Task+CoreDataClass.swift
+++ Reminder-Clone/Model/Task+CoreDataClass.swift
@@ -10,19 +10,33 @@ import UIKit
 import CoreData
 
 enum TaskAttributesKey: String {
-  case title = "title"
-  case notes = "notes"
-  case URL = "link"
-  case date = "date"
-  case time = "time"
-  case location = "location"
-  case flag = "flag"
-  case priority = "priority"
-  case image = "image"
-  case subtasks = "children"
-  case parent = "list"
+	case title = "title"
+	case notes = "notes"
+	case URL = "link"
+	case date = "date"
+	case time = "time"
+	case location = "location"
+	case flag = "flag"
+	case priority = "priority"
+	case image = "image"
+	case subtasks = "children"
+	case parent = "list"
 }
 
 @objc(Task)
 public class Task: NSManagedObject {
+	var dictionary: [TaskAttributesKey: Any] {
+		var dict = [TaskAttributesKey: Any]()
+		dict[.title] = title
+		dict[.notes] = notes
+		dict[.URL] = link
+		dict[.date] = date
+		dict[.time] = time
+		dict[.location] = location
+		dict[.flag] = flag
+		dict[.image] = location
+		dict[.parent] = parent
+		dict[.subtasks] = children
+		return dict
+	}
 }
