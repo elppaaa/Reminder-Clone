23443f22cbb871a4109e8101ff156989e3d51235
elppaaa <withbcd@gmail.com>
2021-04-28 20:03:31 +0900

    [feat] Add insets, tint function for UIImage
    - 아이콘을 만들기 위한 inset, tint 지정 함수 추가

--- Reminder-Clone/Helpers/Extensions/UIImage+Extension.swift
+++ Reminder-Clone/Helpers/Extensions/UIImage+Extension.swift
@@ -17,3 +17,29 @@ extension UIImage {
   static let flagCircle = UIImage(named: "flag.circle.fill")!
   static let flag = UIImage(named: "flag.fill")!
 }
+
+extension UIImage {
+  
+  func with(color: UIColor) -> UIImage {
+    var image = withRenderingMode(.alwaysTemplate)
+    UIGraphicsBeginImageContextWithOptions(size, false, scale)
+    color.set()
+    image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+    image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    return image
+  }
+  
+  func with(insets: UIEdgeInsets) -> UIImage? {
+    let cgSize = CGSize(width: self.size.width + insets.left * self.scale + insets.right * self.scale,
+      height: self.size.height + insets.top * self.scale + insets.bottom * self.scale)
+    
+    UIGraphicsBeginImageContextWithOptions(cgSize, false, self.scale)
+    defer { UIGraphicsEndImageContext() }
+    let origin = CGPoint(x: insets.left * self.scale, y: insets.top * self.scale)
+    self.draw(at: origin)
+    
+    return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(self.renderingMode)
+  }
+  
+}
