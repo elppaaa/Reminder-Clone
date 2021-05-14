2485e395365b853b9342419cecb6b861ad22f915
elppaaa <withbcd@gmail.com>
2021-05-15 01:38:54 +0900

    [feat] Add Notification, UIColor Extension
    - Core Data 에 적절하게 저장하기 위한 타입 변경
    - UIColor 를 hex Int 로 변경하는 부분 추가
    - icon 을 SF Symbol string 으로 변경

---
+++ Reminder-Clone/Helpers/Extensions/Notification+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Extension.swift
+//  Reminder-Clone
+//
+//  Created by JK on 2021/05/15.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let CategoryChanged = Notification.Name(rawValue: "CategoryChanged")
+  static let TaskChanged = Notification.Name(rawValue: "TaskChanged")
+}
--- Reminder-Clone/Helpers/Extensions/UIColor+Extension.swift
+++ Reminder-Clone/Helpers/Extensions/UIColor+Extension.swift
@@ -15,4 +15,15 @@ public extension UIColor {
   convenience init(hex: Int) {
     self.init(r: (hex & 0xff0000) >> 16, g: (hex & 0xff00) >> 8, b: (hex & 0xff), a: 1)
   }
+
+  var hex: Int {
+    var r: CGFloat = 0
+    var g: CGFloat = 0
+    var b: CGFloat = 0
+    var a: CGFloat = 0
+
+    getRed(&r, green: &g, blue: &b, alpha: &a)
+    let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
+    return rgb
+  }
 }
