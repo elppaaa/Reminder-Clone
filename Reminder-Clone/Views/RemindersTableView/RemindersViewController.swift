bbd8dc3e638bbf89e05bb0ffdc6db74df84bc78f
elppaaa <withbcd@gmail.com>
2021-05-30 13:43:28 +0900

    [feat] Add option button on RemindersVC
    - 오른쪽 위 tabBar 버튼 추가
    - UIMenu 는 iOS 14 버전부터 가능함으로 13 은actionSheet 사용

--- Reminder-Clone/Views/RemindersTableView/RemindersViewController.swift
+++ Reminder-Clone/Views/RemindersTableView/RemindersViewController.swift
@@ -35,11 +35,11 @@ class RemindersViewController: UITableViewController {
     super.viewDidLoad()
     view.backgroundColor = R.Color.defaultBackground
     tableView.keyboardDismissMode = .interactive
-    
     configLayout()
     configGesture()
+    configTabBar()
   }
-  
+
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     PersistentManager.shared.saveContext()
@@ -75,7 +75,7 @@ extension RemindersViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(
-      withIdentifier: ReminderTableViewCell.identifier, for: indexPath) as? ReminderTableViewCell else {
+            withIdentifier: ReminderTableViewCell.identifier, for: indexPath) as? ReminderTableViewCell else {
       fatalError("Cell Not Founded")
     }
     
@@ -216,5 +216,52 @@ extension RemindersViewController: UIGestureRecognizerDelegate {
   func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
     if gestureRecognizer is UITapGestureRecognizer, touch.view == tableView { return true }
     return false
+  }
+
+  var menus: [UIAction] {
+    [
+      UIAction(title: "Name & Appearance", image: UIImage(systemName: "pencil"), handler: { _ in print("action called") }),
+      UIAction(title: "Share List", image: UIImage(systemName: "person.crop.circle.fill.badge.plus"), handler: { _ in print("action called") }),
+      UIAction(title: "Select Reminders", image: UIImage(systemName: "checkmark.circle"), handler: { _ in print("action called") }),
+      UIAction(title: "Sort By", image: UIImage(systemName: "arrow.up.arrow.down"), handler: { _ in print("action called") }),
+      UIAction(title: "Show Completed", image: UIImage(systemName: "eye"), handler: { _ in print("action called") }),
+      UIAction(title: "Print", image: UIImage(systemName: "printer"), handler: { _ in print("action called") }),
+      UIAction(title: "Delete List", image: UIImage(systemName: "trash"), attributes: .destructive, handler: { _ in print("action called") }),
+    ]
+  }
+}
+
+// MARK: - UITabBar Menu
+extension RemindersViewController {
+
+  func configTabBar() {
+    if #available(iOS 14.0, *) {
+      navigationItem.rightBarButtonItem = UIBarButtonItem(
+        title: nil,
+        image: UIImage(systemName: "ellipsis.circle"),
+        primaryAction: nil,
+        menu: UIMenu(children: menus))
+    } else {
+      navigationItem.rightBarButtonItem =
+        UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: #selector(didRightBarButtonTapped))
+    }
+  }
+
+  func createAction() {
+
+  }
+
+  @objc
+  func didRightBarButtonTapped() {
+    let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    alert.addAction( UIAlertAction(title: "Name & Appearance", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Share List", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Select Reminders", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Sort By", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Show Completed", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Print", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Delete List", style: .default, handler: { _ in print("action") }) )
+    alert.addAction( UIAlertAction(title: "Cancel", style: .cancel, handler: nil) )
+    present(alert, animated: true, completion: nil)
   }
 }
